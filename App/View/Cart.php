bfe38c718f229d255561ecf95a6097692476807c
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-11 01:05:50 -0300

    CDN boostrap

--- App/View/Cart.php
+++ App/View/Cart.php
@@ -2,14 +2,15 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
-	<title>VIEW CARRINHO</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>Carrinho de Compras PHP/POO</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
 <body>
 	<div class="container">
 		<div class="page-header">
-  			<h1>Carrinho - View</h1>
+  			<h1>Carrinho</h1>
 		</div>
 
 		<table class="table">
@@ -20,6 +21,7 @@
 					<th>Quantidade</th>
 					<th>Preço</th>
 					<th>Subtotal</th>
+                    <th></th>
 				</tr>
 			</thead>
 			<tbody>
@@ -34,6 +36,12 @@
 			</tbody>
 		</table>
 	</div>
-	
+
+
+
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
