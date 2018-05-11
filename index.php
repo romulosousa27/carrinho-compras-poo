b0c5150b74ec837ddd0c98074d7fbb77f7ffc4c6
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-11 01:06:47 -0300

    interface repository, banco, uses

--- index.php
+++ index.php
@@ -3,10 +3,18 @@
 define("DIR", dirname(__FILE__));
 define("DS", DIRECTORY_SEPARATOR);
 
+use App\Controller\Cart;
+use App\Controller\Home;
+use App\Loader;
+use App\Model\Product\ProductRepositoryPDO;
+
 include_once DIR.DS.'App'.DS.'Loader.php';
 
-$loader = new \App\Loader();
+$loader = new Loader();
 $loader->register();
+
+$pdo = new \PDO('mysql:host=localhost; dbname=shop', 'root', '');
+$productRepository = new ProductRepositoryPDO($pdo);
 
 //testa se o page e o action tem parametros, e retorna.
 $page 	= isset($_GET['page']) ? $_GET['page'] : '';
@@ -15,12 +23,14 @@ $action = isset($_GET['action']) ? $_GET['action'] : 'index';
 
 switch ($page) {
 	case 'cart':
-		$cart = new App\Controller\Cart();
+	    //instancia
+		$cart = new Cart();
 		call_user_func_array(array($cart, $action), array());
 		break;
 	
 	default:
-		$home = new App\Controller\Home();
+        //instancia
+		$home = new Home($productRepository);
 		call_user_func_array(array($home, $action), array());
 		break;
 }
