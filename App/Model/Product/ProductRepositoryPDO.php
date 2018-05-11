407965e8b380ab40b8fb9a0f60991b0fb008af52
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-11 01:07:45 -0300

    conexao e consulta de dados do banco

---
+++ App/Model/Product/ProductRepositoryPDO.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Model\Product;
+
+use PDO;
+
+class ProductRepositoryPDO implements ProductRepository {
+    private $pdo;
+
+    public function __construct(PDO $pdo){
+        $this->pdo = $pdo;
+    }
+
+    public function getProducts(){
+        $stmt = $this->pdo->prepare("SELECT * FROM product");
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'App\Model\Product\Product');
+        $stmt->execute();
+
+        return $stmt->fetchAll();
+    }
+
+    public function getProduct($id){
+        $stmt = $this->pdo->prepare("SELECT * FROM product WHERE id = :id");
+        $stmt->blindValue(":id", $id, PDO::PARAM_INT);
+        $stmt->setFetchMode(PDO::FETCH_CLASS, 'App\Model\Product\Product');
+        $stmt->execute();
+
+        return $stmt->fetch();
+    }
+
+}
