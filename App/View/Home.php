3ccd01be8411f2c2c3bd9cd89b1f2b3e074fe0d5
Romulo Sousa <romulosousa27@hotmail.com>
2018-05-11 01:06:04 -0300

    CDN boostrap

--- App/View/Home.php
+++ App/View/Home.php
@@ -2,17 +2,18 @@
 <html lang="pt-br">
 <head>
 	<meta charset="UTF-8">
-	<title>VIEW HOME</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<title>Carrinho de Compras PHP/POO</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
 </head>
 <body>
 	<div class="container">
 		<div class="page-header">
-  			<h1>Home - View</h1>
+  			<h1>Home</h1>
 		</div>
 
-		<table class="table">
+		<table class="table table-striped">
 			<thead>
 				<tr>
 					<th>#</th>
@@ -31,5 +32,11 @@
 			</tbody>
 		</table>
 	</div>
+
+
+
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
 </html>
